aa206cecb5292892c2d6787a4281beccc56af91a
nashiusagi <ritotech0226@gmail.com>
2024-05-03 23:44:11 +0900

    [update] cleanup部分は別ロジック

--- src/lib/useParser.ts
+++ src/lib/useParser.ts
@@ -4,14 +4,23 @@ export const useParser = async (body: string) => {
   const root = await parse(body)
   const articles = root?.querySelectorAll(".Box-row")
 
-  const trends = articles.map((element) => {
+  const trends = articles.map((element, idx) => {
     const textDirty = String(element.querySelector("h2")?.innerText)
-    const text = textDirty?.replace(/\n/g, '').replace(/\s/g, '')
+    const repoName = textDirty ? cleanInnerText(textDirty) : ""
+
     return {
-      title: text,
-      link: `https://github.com/${text}`
+      rank: idx,
+      title: repoName,
+      link: `https://github.com/${repoName}`
     }
   })
 
   return trends
+}
+
+/**
+ * innerTextにはspanタグなど由来の\nや\sが紛れ込むので取っ払う
+ */
+const cleanInnerText = (innerText: string): string => {
+  return innerText.replace(/\n/g, '').replace(/\s/g, '')
 }
